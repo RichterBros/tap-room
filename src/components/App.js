128bce6812b0e7919d1ba67d7373a763fa692236
Erich Richter <erich.richter@gmail.com>
2020-09-18 11:51:46 -0700

    add styling

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Header";
 import KegControl from "./KegControl";
 
-
+import { Container, Row, Col, Jumbotron } from 'react-bootstrap';
 
 function App(){
   return ( 
    
     
     <React.Fragment>
+      <Container>
+        <Jumbotron>
+    
     <p>React Bootstap</p>
       <Header />
       <KegControl />
       
+      </Jumbotron>
+      </Container>
     </React.Fragment>
   );
 }
